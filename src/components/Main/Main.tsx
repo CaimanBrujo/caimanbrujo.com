b95601b5c53b2c974acff87e70b1ee7024346bb2
Caimanbrujo <186770762+CaimanBrujo@users.noreply.github.com>
2025-07-04 14:50:19 +0200

    style: adjust top and bottom margins for Stack section on mobile

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -10,10 +10,11 @@ export default function Main() {
       className="relative bg-background z-40 mt-[100vh] flex-1 flex flex-col items-center justify-start min-h-[100vh]"
     >
       <div id="stack"></div>
-      <div className="flex bg-background flex-col mt-30 mb-42 md:flex-row md:justify-between w-full max-w-[1200px] gap-8 md:gap-0">
+      <div className="flex bg-background flex-col mt-16 md:mt-30 mb-20 md:mb-42 md:flex-row md:justify-between w-full max-w-[1200px] gap-8 md:gap-0">
         <StackCard />
         <SphereIcons />
       </div>
+
       <StacksAnimated />
       <Projects />
     </main>
